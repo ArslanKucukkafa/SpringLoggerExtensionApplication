a6da83c20cf90b4a64c1a6e2cfb05a5d2ddaea90
Arslan Kucukkafa <arslankucukkafa@gmail.com>
2025-01-20 23:30:36 +0300

    first commit

---
+++ RestLogger/src/test/java/com/arslankucukkafa/dev/restlogger/RestLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arslankucukkafa.dev.restlogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestLoggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
